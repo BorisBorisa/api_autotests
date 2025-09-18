4c2e0303b30e47276c6c17739222f4d3054482ad
Aleksandr Borisov <BorisaGH@yandex.ru>
2025-09-18 18:43:48 +0300

    add config

---
+++ config.py
@@ -0,0 +1,30 @@
+from pydantic import BaseModel, HttpUrl
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class HTTTPClientConfig(BaseModel):
+    url: HttpUrl
+    timeout: float
+
+    @property
+    def client_url(self) -> str:
+        return str(self.url)
+
+
+class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        extra="allow",
+        env_file=".env",
+        env_file_encoding="utf-8",
+        env_nested_delimiter="."
+    )
+
+    http_client: HTTTPClientConfig
+
+
+
+if __name__ == '__main__':
+    tmp = Settings()
+
+    print(tmp)
+
