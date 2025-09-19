e23f84fd9d4eaab936e61f4d8f05106538e0ff28
Aleksandr Borisov <BorisaGH@yandex.ru>
2025-09-19 19:00:06 +0300

    add new data generation helpers

--- tools/fakers.py
+++ tools/fakers.py
@@ -1,5 +1,6 @@
 from faker import Faker
-from random import choice
+from random import choice, randint
+
 
 class Fake:
     """
@@ -46,6 +47,42 @@ class Fake:
         """
         return self.faker.sentence()
 
+    def phrase(self) -> str:
+        """
+        Генерирует случайное словосочетание.
+
+        :return: Случайное словосочетание.
+        """
+        return self.faker.sentence(nb_words=3, variable_nb_words=True)[:-1]
+
+    def uri(self) -> str:
+        """
+        Генерирует случайное URI.
+
+        :return: Случайный URI.
+        """
+        return self.faker.uri()
+
+    def uris_list(self) -> list[str]:
+        """
+        Генерирует словарь случайных URI.
+
+        :return: Словарь со случайными URI.
+        """
+        return [self.uri() for _ in range(randint(1, 4))]
+
+    def price(self) -> float:
+        """
+        Генерирует случайную цену.
+
+        :return: Случайная цена (float).
+        """
+        return self.faker.pyfloat(
+            right_digits=2,
+            min_value=1,
+            max_value=10000
+        )
+
     @staticmethod
     def role() -> str:
         """
@@ -53,7 +90,7 @@ class Fake:
 
         :return: Случайная роль из допустимых.
         """
-        roles = ("admin", "customer") # Перенести в конфиг!
+        roles = ("admin", "customer")  # Перенести в конфиг!
         return choice(roles)
 
 
