c4e5ed116a3f78d425c3eafe58467bbb7d1efd79
Aleksandr Borisov <BorisaGH@yandex.ru>
2025-09-21 01:12:24 +0300

    add error assert

---
+++ tools/assertions/errors.py
@@ -0,0 +1,19 @@
+from clients.errors_schema import ErrorResponseSchema
+from tools.assertions.base import assert_equal
+
+
+def assert_error_response(
+        actual: ErrorResponseSchema,
+        expected: ErrorResponseSchema
+):
+    """
+    Проверяет, что объект ответа API с ошибками валидации (`ErrorResponseSchema`)
+    соответствует ожидаемому значению.
+
+    :param actual: Фактический ответ API.
+    :param expected: Ожидаемый ответ API.
+    :raises AssertionError: Если значения полей не совпадают.
+    """
+
+    assert_equal(actual.status_code, expected.status_code, "status_code")
+    assert_equal(actual.message, actual.message, "message")
