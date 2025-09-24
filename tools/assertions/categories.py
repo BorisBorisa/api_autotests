8f69fe8d93802604ab046516872d009789bd16d9
Aleksandr Borisov <BorisaGH@yandex.ru>
2025-09-24 16:39:13 +0300

    add create category with wrong data response assert

--- tools/assertions/categories.py
+++ tools/assertions/categories.py
@@ -2,7 +2,9 @@ from clients.categories.categories_schema import (
     CreateCategoryRequestSchema,
     CreateCategoryResponseSchema
 )
+from clients.errors_schema import ErrorResponseSchema
 from tools.assertions.base import assert_equal
+from tools.assertions.errors import assert_error_response
 
 
 def assert_create_category_response(request: CreateCategoryRequestSchema, response: CreateCategoryResponseSchema):
@@ -15,3 +17,19 @@ def assert_create_category_response(request: CreateCategoryRequestSchema, respon
     """
     assert_equal(request.name, response.name, "name")
     assert_equal(request.image, str(response.image), "image")
+
+
+def assert_create_category_with_wrong_data_response(actual: ErrorResponseSchema, error_messages: list):
+    """
+    Проверяет, что ответ на обновление данных пользователя с некорректным данными соответствует ожидаемому.
+
+    :param actual: Ответ от API с ошибкой, которую необходимо проверить.
+    :param error_messages: Список сообщений ошибок
+    :return: AssertionError: Если хотя бы одно поле не совпадает.
+    """
+    expected = ErrorResponseSchema(
+        message=error_messages,
+        error="Bad Request",
+        status_code=400
+    )
+    assert_error_response(actual, expected)
