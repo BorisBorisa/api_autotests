e3ef91adde1e10c47d1521fa1b6b8abdc4a66f0e
Aleksandr Borisov <BorisaGH@yandex.ru>
2025-09-25 21:01:53 +0300

    add create product with wrong data response assert

--- tools/assertions/products.py
+++ tools/assertions/products.py
@@ -1,8 +1,10 @@
+from clients.errors_schema import ErrorResponseSchema
 from clients.products.products_schema import (
     CreateProductRequestSchema,
     CreateProductResponseSchema
 )
 from tools.assertions.base import assert_equal
+from tools.assertions.errors import assert_invalid_data_response
 
 
 def assert_create_product_response(request: CreateProductRequestSchema, response: CreateProductResponseSchema):
@@ -18,3 +20,13 @@ def assert_create_product_response(request: CreateProductRequestSchema, response
     assert_equal(request.description, response.description, "description")
     assert_equal(request.category_id, response.category.id, "category id")
     assert_equal(request.images, [str(i) for i in response.images], "images")
+
+def assert_create_product_with_wrong_data_response(actual: ErrorResponseSchema, error_messages: list):
+    """
+    Проверяет, что ответ на создание продукта с некорректным данными соответствует ожидаемому.
+
+    :param actual: Ответ от API с ошибкой, которую необходимо проверить.
+    :param error_messages: Список сообщений ошибок.
+    :return: AssertionError: Если хотя бы одно поле не совпадает.
+    """
+    assert_invalid_data_response(actual, error_messages)
