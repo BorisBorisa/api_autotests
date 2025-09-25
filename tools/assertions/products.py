fc5525a0e9782b1707abd8170290345ae9e1792f
Aleksandr Borisov <BorisaGH@yandex.ru>
2025-09-26 01:32:34 +0300

    add get product assert

--- tools/assertions/products.py
+++ tools/assertions/products.py
@@ -2,6 +2,9 @@ from clients.errors_schema import ErrorResponseSchema
 from clients.products.products_schema import (
     CreateProductRequestSchema,
     CreateProductResponseSchema
+    CreateProductResponseSchema,
+    ProductSchema,
+    GetProductResponseSchema,
 )
 from tools.assertions.base import assert_equal
 from tools.assertions.errors import assert_invalid_data_response
@@ -21,6 +24,7 @@ def assert_create_product_response(request: CreateProductRequestSchema, response
     assert_equal(request.category_id, response.category.id, "category id")
     assert_equal(request.images, [str(i) for i in response.images], "images")
 
+
 def assert_create_product_with_wrong_data_response(actual: ErrorResponseSchema, error_messages: list):
     """
     Проверяет, что ответ на создание продукта с некорректным данными соответствует ожидаемому.
@@ -30,3 +34,33 @@ def assert_create_product_with_wrong_data_response(actual: ErrorResponseSchema, 
     :return: AssertionError: Если хотя бы одно поле не совпадает.
     """
     assert_invalid_data_response(actual, error_messages)
+
+
+def assert_product(actual: ProductSchema, expected: ProductSchema):
+    """
+    Проверяет, что фактические данные продукта соответствуют ожидаемым.
+
+    :param actual: Фактические данные продукта.
+    :param expected: Ожидаемые данные продукта.
+    :return: AssertionError: Если хотя бы одно поле не совпадает.
+    """
+    assert_equal(actual.id, expected.id, "id")
+    assert_equal(actual.title, expected.title, "title")
+    assert_equal(actual.slug, expected.slug, "slug")
+    assert_equal(actual.price, expected.price, "price")
+    assert_equal(actual.description, expected.description, "description")
+    assert_equal(actual.category, expected.category, "category")
+    assert_equal(actual.images, expected.images, "images")
+    assert_equal(actual.creation_at, expected.creation_at, "creation_at")
+    assert_equal(actual.updated_at, expected.updated_at, "updated_at")
+
+
+def assert_get_product_response(actual: GetProductResponseSchema, expected: CreateProductResponseSchema):
+    """
+    Проверяет, что ответ на запрос продукта совпадает с ответом при её создании
+
+    :param actual: Фактические данные продукта.
+    :param expected: Ожидаемые данные продукта.
+    :return: AssertionError: Если хотя бы одно поле не совпадает.
+    """
+    assert_product(actual, expected)
