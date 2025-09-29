ce4f79092e6256e1f34e8e410e774ce7fac30c96
Aleksandr Borisov <BorisaGH@yandex.ru>
2025-09-29 20:28:06 +0300

    refactor routes constants

--- tools/routes.py
+++ tools/routes.py
@@ -2,12 +2,12 @@ from enum import Enum
 
 
 class APIRoutes(str, Enum):
-    PRODUCTS = "/api/v1/products"
-    USERS = "/api/v1/users"
-    AUTH = "/api/v1/auth"
-    CATEGORIES = "/api/v1/categories"
-    FILES = "/api/v1/files"
-    LOCATIONS = "/api/v1/locations"
+    PRODUCTS = "api/v1/products"
+    USERS = "api/v1/users"
+    AUTH = "api/v1/auth"
+    CATEGORIES = "api/v1/categories"
+    FILES = "api/v1/files"
+    LOCATIONS = "api/v1/locations"
 
     def __str__(self):
         return self.value
