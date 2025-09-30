af0b81f5d4fd243f2bd729d3bf752a9a199f8e03
Aleksandr Borisov <BorisaGH@yandex.ru>
2025-09-30 03:51:38 +0300

    add locations response len assert

---
+++ tools/assertions/locations.py
@@ -0,0 +1,16 @@
+from clients.locations.locations_schema import GetLocationsResponseSchema
+
+
+def assert_get_locations_response_len(response: GetLocationsResponseSchema, expected_len: int):
+    """
+    Проверяет что фактическая длинна ответа соответстввует ожидаемому.
+
+    :param response: Ответ от API при запросе списка адресов.
+    :param expected_len: Ожидаемая длинна ответа
+    :return:
+    """
+    assert len(response) == expected_len, (
+        f'Incorrect get locations response length. '
+        f'Expected length: {expected_len}. '
+        f'Actual length: {len(response)}'
+    )
