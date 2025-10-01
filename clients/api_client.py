6d522673acd7497478079e7fe135ac460055602e
Aleksandr Borisov <BorisaGH@yandex.ru>
2025-10-01 18:23:59 +0300

    add allure steps to base api client

--- clients/api_client.py
+++ clients/api_client.py
@@ -1,3 +1,5 @@
+import allure
+
 from httpx import Client, URL, Response
 from typing import Any
 
@@ -13,6 +15,7 @@ class APIClient:
         """
         self.client = client
 
+    @allure.step("Make GET request to {url}")
     def get(self, url: URL | str, params: QueryParamTypes | None = None) -> Response:
         """
         Выполняет GET-запрос.
@@ -23,6 +26,7 @@ class APIClient:
         """
         return self.client.get(url, params=params)
 
+    @allure.step("Make POST request to {url}")
     def post(
             self,
             url: URL | str,
@@ -41,6 +45,7 @@ class APIClient:
         """
         return self.client.post(url, json=json, data=data, files=files)
 
+    @allure.step("Make PUT request to {url}")
     def put(self, url: URL | str, json: Any | None = None) -> Response:
         """
         Выполняет PUT-запрос (полная замена ресурса).
@@ -51,6 +56,7 @@ class APIClient:
         """
         return self.client.put(url, json=json)
 
+    @allure.step("Make PATCH request to {url}")
     def patch(self, url: URL | str, json: Any | None = None) -> Response:
         """
         Выполняет PATCH-запрос (частичное обновление данных).
@@ -61,6 +67,7 @@ class APIClient:
         """
         return self.client.patch(url, json=json)
 
+    @allure.step("Make DELETE request to {url}")
     def delete(self, url: URL | str) -> Response:
         """
         Выполняет DELETE-запрос (удаление данных).
